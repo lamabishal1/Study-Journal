487e4ca9c38d0911b37eedad281786364c717aaf
ラマ　ビサル <lb1627565@gmail.com>
2025-05-15 18:07:51 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+"use strict";
+let a = 12;
+let b = 2;
+function abcd() {
+    return (a * b);
+}
+//Basic Types
+// Primitive types (number, string, boolean)
+// Arrays []
+//Tuples
+//Enums
+//Any, Unknown, Void, Null, Undefined, Never
+// primitives and reference
+// reference are data inside this {} () []
+// any changes made inside the bracket also makes the change in the parent element
+//Tuples
+//let arr: [string, number] = ["bihshal", 23];
+//let arr2:[number, string] = [12,"bishal"];
+
+//enumerations = same as object with special powers
+
+// enum UserRoles {
+//     ADMIN = "admin",
+//     GUEST = "guest",
+//     SUPER_ADMIN = "super_admin"
+// }
+// UserRoles.ADMIN
