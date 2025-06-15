0d9c791b90af5dea3e25d969cc0c782258d44479
LAMA BISHAL <lb1627565@gmail.com>
2025-06-15 20:08:12 +0900

    okei

--- server2.js
+++ server2.js
@@ -3,7 +3,7 @@ import  { createServer } from 'http';
 const PORT = process.env.PORT;
 
 const users = [
-    {id:1, name: 'Bishal'},
+    {id:1, name: 'Bishalu'},
     {id:2, name: 'Lama'},
     {id:3, name: 'Rahul'},
     {id:4, name: 'Oren'},
